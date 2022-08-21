9442a0025b89478e315673a270e32afc6baec3cf
Jonathan Irvin <djfoxyslpr@gmail.com>
2022-08-21 16:24:03 -0500

    fix prop types

--- src/MealDate.jsx
+++ src/MealDate.jsx
@@ -1,7 +1,7 @@
 import { Box, Heading, Text } from 'grommet';
 import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import { PropTypes, forbidExtraProps } from 'prop-types';
 
 export default function MealDate({
   date,
@@ -80,10 +80,10 @@ export default function MealDate({
   );
 }
 
-MealDate.propTypes = {
+MealDate.propTypes = forbidExtraProps({
   date: PropTypes.string.isRequired,
   direction: PropTypes.number.isRequired,
   breakfast: PropTypes.string.isRequired,
   lunch: PropTypes.string.isRequired,
   dinner: PropTypes.string.isRequired,
-};
+});
