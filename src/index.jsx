8ec2c67ca2d244c0501f991187f932f71ab70f18
Jonathan Irvin <djfoxyslpr@gmail.com>
2022-08-27 17:37:57 -0500

    organize imports

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '@fontsource/roboto';
-import App from './App';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
+import App from './App';
 
 Amplify.configure(awsconfig);
 
