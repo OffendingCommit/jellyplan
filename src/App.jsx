439184a95ee8a1e6685c2b86d5c29dfa103e5d4a
Jonathan Irvin <djfoxyslpr@gmail.com>
2022-09-03 16:23:16 -0500

    feat: add packages to support UI with figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-param-reassign */
 import React from 'react';
 import { Grommet } from 'grommet';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import theme from './theme';
-import Home from './scenes/Home/Home';
 
-function App() {
+import Home from './scenes/Home/Home';
+import '@aws-amplify/ui-react/styles.css';
+import { PropTypes } from 'prop-types';
+import MealDate from './components/MealDate/MealDate';
+
+function App({ signOut, user }) {
   return (
     <Grommet theme={theme}>
       <Home />
@@ -12,4 +17,9 @@ function App() {
   );
 }
 
-export default App;
+App.propTypes = {
+  user: PropTypes.object.isRequired,
+  signOut: PropTypes.func.isRequired,
+};
+
+export default withAuthenticator(App);
