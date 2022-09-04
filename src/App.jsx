698a43ef258fa93bff1a0e4dde81d08f91338097
Jonathan Irvin <djfoxyslpr@gmail.com>
2022-09-04 17:22:34 -0500

    fix: add proper authentication wiring

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-/* eslint-disable no-param-reassign */
-import React from 'react';
-// import { Grommet } from 'grommet';
-import { Authenticator } from '@aws-amplify/ui-react';
-// import theme from './theme';
-// import Home from './scenes/Home/Home';
-import { PropTypes } from 'prop-types';
+import { Amplify } from 'aws-amplify';
 
-function App() {
+import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <ThemeProvider>
+      <Authenticator
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </ThemeProvider>
   );
 }
-
-App.propTypes = {
-  user: PropTypes.object.isRequired,
-  signOut: PropTypes.func.isRequired,
-};
-
-export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,63 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
-import eslint from 'vite-plugin-eslint';
-import legacy from '@vitejs/plugin-legacy';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [
-      eslint({
-        fix: true,
-      }),
-      svgrPlugin({
-        svgrOptions: {
-          icon: true,
-        },
-      }),
-      react({
-        babel: {
-          babelrc: false,
-          configFile: false,
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
-      }),
-      legacy({
-        targets: ['defaults', 'not IE 11'],
-      }),
-    ],
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-          global: 'globalThis', //<-- AWS SDK
-        },
-        plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
-      },
-    },
-    define: {
-      'process.env.NODE_ENV': `"${mode}"`,
-    },
-    build: {
-      outDir: 'build',
-      rollupOptions: {
-        plugins: [
-          // Enable rollup polyfills plugin
-          // used during production bundling
-          nodePolyfills(),
-        ],
-      },
-    },
-    resolve: {
-      alias: [
-        {
-          find: './runtimeConfig',
-          replacement: './runtimeConfig.browser',
-        },
-      ],
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build',
+  },
+});
