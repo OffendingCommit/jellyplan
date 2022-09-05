72378655c296b10d6238ee6bd6f1c3c9d427283f
Jonathan Irvin <djfoxyslpr@gmail.com>
2022-09-04 21:12:29 -0500

    fix: build polyfill issues

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,43 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
+import builtins from 'rollup-plugin-node-builtins';
+import legacy from '@vitejs/plugin-legacy';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    splitVendorChunkPlugin(),
+    eslint(),
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
   },
+  build: {
+    outDir: 'publish',
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        builtins(),
+        rollupNodePolyFill(),
+      ],
+    },
+  },
+  resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } },
 });
